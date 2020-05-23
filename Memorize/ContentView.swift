1b3f53bc32e22e1b5600ea91c284434d5c16304c
Charles Emley <ce_social@icloud.com>
2020-05-22 19:08:17 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Charles Emley on 5/22/20.
+//  Copyright © 2020 Charles Emley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
