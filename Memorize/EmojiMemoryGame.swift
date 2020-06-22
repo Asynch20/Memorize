b18284f2c18da2af2aceb14611fc2e46759c1373
Charles Emley <ce_social@icloud.com>
2020-06-22 09:01:53 -0500

    Initial codebase, pre Lecture 2 homework

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,38 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Charles Emley on 6/18/20.
+//  Copyright © 2020 Charles Emley. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["🍷","🍸","🥃"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    //MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    //MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
+
+struct EmojiMemoryGame_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
